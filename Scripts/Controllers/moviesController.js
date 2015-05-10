f3108db2e01f1c352b1388429f7f12603bdaf756
Cleophas Mashiri <cleophasmashiri@gmail.com>
2015-05-10 17:13:49 +0200

    move sol file

---
+++ Scripts/Controllers/moviesController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('moviesController', moviesController);
+
+    moviesController.$inject = ['$location']; 
+
+    function moviesController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'moviesController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
